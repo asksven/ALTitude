27337467d727ff9b58c540a5a01bc13dc8eaa0f1
asksven <sven.knispel@gmail.com>
2012-06-04 07:41:16 +0200

    Fixed #16: [feature] allow to change settings from tasker https://github.com/asksven/ALTitude/issues/issue/16

--- gen/com/asksven/betterlatitude/R.java
+++ gen/com/asksven/betterlatitude/R.java
@@ -11,98 +11,163 @@ public final class R {
     public static final class array {
         /**  Values for the update precision. 
          */
-        public static final int accuracyLabels=0x7f06000a;
-        public static final int accuracyValues=0x7f06000b;
-        public static final int intervalLabels=0x7f060008;
-        public static final int intervalValues=0x7f060009;
-        public static final int locProviderLabels=0x7f060006;
-        public static final int locProviderValues=0x7f060007;
+        public static final int accuracyLabels=0x7f09000b;
+        public static final int accuracyValues=0x7f09000c;
+        public static final int intervalLabels=0x7f090009;
+        public static final int intervalValues=0x7f09000a;
+        public static final int locProviderLabels=0x7f090007;
+        public static final int locProviderValues=0x7f090008;
         /**  Values for the update precision. 
          */
-        public static final int mapLocProviderAccuracyLabels=0x7f060010;
-        public static final int mapLocProviderAccuracyValues=0x7f060011;
-        public static final int mapLocProviderIntervalLabels=0x7f06000e;
-        public static final int mapLocProviderIntervalValues=0x7f06000f;
-        public static final int mapLocProviderLabels=0x7f06000c;
-        public static final int mapLocProviderValues=0x7f06000d;
-        public static final int quickAccuracyLabels=0x7f060002;
-        public static final int quickAccuracyValues=0x7f060003;
-        public static final int quickDurationLabels=0x7f060004;
-        public static final int quickDurationValues=0x7f060005;
+        public static final int mapLocProviderAccuracyLabels=0x7f090011;
+        public static final int mapLocProviderAccuracyValues=0x7f090012;
+        public static final int mapLocProviderIntervalLabels=0x7f09000f;
+        public static final int mapLocProviderIntervalValues=0x7f090010;
+        public static final int mapLocProviderLabels=0x7f09000d;
+        public static final int mapLocProviderValues=0x7f09000e;
+        public static final int quickAccuracyLabels=0x7f090003;
+        public static final int quickAccuracyValues=0x7f090004;
+        public static final int quickActionLabels=0x7f090000;
+        public static final int quickDurationLabels=0x7f090005;
+        public static final int quickDurationValues=0x7f090006;
         /**  Values for the update interval. 
          */
-        public static final int quickIntervalLabels=0x7f060000;
-        public static final int quickIntervalValues=0x7f060001;
+        public static final int quickIntervalLabels=0x7f090002;
+        public static final int quickIntervalValues=0x7f090001;
     }
     public static final class attr {
     }
     public static final class drawable {
-        public static final int icon=0x7f020000;
+        public static final int ic_launcher=0x7f020000;
+        public static final int icon=0x7f020001;
+        public static final int icon_dontsave=0x7f020002;
+        public static final int icon_help=0x7f020003;
+        public static final int icon_save=0x7f020004;
+        public static final int twofortyfouram_locale_border=0x7f020005;
     }
     public static final class id {
-        public static final int ButtonCancel=0x7f090012;
-        public static final int ButtonOk=0x7f090011;
-        public static final int ButtonReset=0x7f090013;
-        public static final int adView=0x7f09000b;
-        public static final int buttonClose=0x7f090015;
-        public static final int buttonLogon=0x7f09000a;
-        public static final int buttonMarket=0x7f090017;
-        public static final int buttonTwitter=0x7f090016;
-        public static final int call_latitude=0x7f09001a;
-        public static final int image=0x7f090001;
-        public static final int imageView1=0x7f090008;
-        public static final int layout_root=0x7f090000;
-        public static final int location_Status=0x7f09001c;
-        public static final int log_off=0x7f09001e;
-        public static final int mainlayout=0x7f09000c;
-        public static final int mapview=0x7f09000d;
-        public static final int preferences=0x7f090018;
-        public static final int quick_dialog=0x7f09001b;
-        public static final int release_notes=0x7f09001d;
-        public static final int show_map=0x7f090019;
-        public static final int spinnerAccuracy=0x7f09000f;
-        public static final int spinnerDuration=0x7f090010;
-        public static final int spinnerInterval=0x7f09000e;
-        public static final int text=0x7f090003;
-        public static final int textView1=0x7f090007;
-        public static final int textViewHint=0x7f090005;
-        public static final int textViewName=0x7f090004;
-        public static final int textViewStatus=0x7f090009;
-        public static final int textViewVersion=0x7f090006;
-        public static final int title=0x7f090002;
-        public static final int toggle_sat=0x7f09001f;
-        public static final int webview=0x7f090014;
+        public static final int ButtonCancel=0x7f050017;
+        public static final int ButtonOk=0x7f050016;
+        public static final int ButtonReset=0x7f050018;
+        public static final int TextView03=0x7f050007;
+        public static final int adView=0x7f050013;
+        public static final int buttonClose=0x7f05001a;
+        public static final int buttonLogon=0x7f050012;
+        public static final int buttonMarket=0x7f05001c;
+        public static final int buttonTwitter=0x7f05001b;
+        public static final int call_latitude=0x7f05001f;
+        public static final int image=0x7f050004;
+        public static final int imageView1=0x7f050010;
+        public static final int layout_root=0x7f050003;
+        public static final int location_Status=0x7f050021;
+        public static final int log_off=0x7f050023;
+        public static final int mainlayout=0x7f050014;
+        public static final int mapview=0x7f050015;
+        public static final int preferences=0x7f05001d;
+        public static final int quick_dialog=0x7f050020;
+        public static final int release_notes=0x7f050022;
+        public static final int show_map=0x7f05001e;
+        public static final int spinnerAccuracy=0x7f05000a;
+        public static final int spinnerAction=0x7f050008;
+        public static final int spinnerDuration=0x7f05000b;
+        public static final int spinnerInterval=0x7f050009;
+        public static final int text=0x7f050006;
+        public static final int textView1=0x7f05000f;
+        public static final int textViewHint=0x7f05000d;
+        public static final int textViewName=0x7f05000c;
+        public static final int textViewStatus=0x7f050011;
+        public static final int textViewVersion=0x7f05000e;
+        public static final int title=0x7f050005;
+        public static final int toggle_sat=0x7f050024;
+        /**  Menu item to cancel the current Activity 
+         */
+        public static final int twofortyfouram_locale_menu_dontsave=0x7f050001;
+        /**  Menu item to launch the Help 
+         */
+        public static final int twofortyfouram_locale_menu_help=0x7f050000;
+        /**  Menu item to save the current Activity 
+         */
+        public static final int twofortyfouram_locale_menu_save=0x7f050002;
+        public static final int webview=0x7f050019;
+    }
+    public static final class integer {
+        /**  The maximum blurb length in characters.  This value is based on Latin characters 
+         */
+        public static final int twofortyfouram_locale_maximum_blurb_length=0x7f060000;
     }
     public static final class layout {
         public static final int information_dialog=0x7f030000;
-        public static final int main=0x7f030001;
-        public static final int main_old=0x7f030002;
-        public static final int map_debug=0x7f030003;
-        public static final int map_release=0x7f030004;
-        public static final int quick_action_dialog=0x7f030005;
-        public static final int readmewebview=0x7f030006;
-        public static final int webview=0x7f030007;
+        public static final int locale_plugin_main=0x7f030001;
+        public static final int main=0x7f030002;
+        public static final int main_old=0x7f030003;
+        public static final int map_debug=0x7f030004;
+        public static final int map_release=0x7f030005;
+        public static final int quick_action_dialog=0x7f030006;
+        public static final int readmewebview=0x7f030007;
+        public static final int webview=0x7f030008;
     }
     public static final class menu {
-        public static final int mainmenu=0x7f080000;
-        public static final int mapmenu=0x7f080001;
+        public static final int mainmenu=0x7f0a0000;
+        public static final int mapmenu=0x7f0a0001;
+        public static final int twofortyfouram_locale_help_save_dontsave=0x7f0a0002;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int hello=0x7f050000;
-        public static final int menu_change_precision=0x7f050005;
-        public static final int menu_log_off=0x7f050008;
-        public static final int menu_preferences=0x7f050002;
-        public static final int menu_release_notes=0x7f050007;
-        public static final int menu_show_friends=0x7f050004;
-        public static final int menu_show_on_map=0x7f050003;
-        public static final int menu_status=0x7f050006;
-        public static final int menu_toggle_sat=0x7f050009;
+        public static final int app_name=0x7f070001;
+        public static final int hello=0x7f070000;
+        public static final int label_select_accuracy=0x7f070016;
+        public static final int label_select_action=0x7f070014;
+        public static final int label_select_duration=0x7f070017;
+        public static final int label_select_interval=0x7f070015;
+        public static final int menu_change_precision=0x7f07000e;
+        public static final int menu_log_off=0x7f070011;
+        public static final int menu_preferences=0x7f07000b;
+        public static final int menu_release_notes=0x7f070010;
+        public static final int menu_show_friends=0x7f07000d;
+        public static final int menu_show_on_map=0x7f07000c;
+        public static final int menu_status=0x7f07000f;
+        public static final int menu_toggle_sat=0x7f070012;
+        public static final int plugin_name=0x7f070013;
+        /**  A Toast message that appears if an application that is being launched isn't available.  There is no maximum length, although this message will only appear on the screen for about 5 seconds in a Toast message so it should be short enough to be comprehended before it disappears. 
+         */
+        public static final int twofortyfouram_locale_application_not_available=0x7f070002;
+        /**  A format string for breadcrumbs in the titlebar.  For left to right languages, such as English, the format will look something like "Main Activity > Sub Activity > Sub Activity 2".  Item #1 is the previous path, item #2 is the separator, and item #3 is the current item.  For most left to right languages, this shouldn't need to be changed. 
+         */
+        public static final int twofortyfouram_locale_breadcrumb_format=0x7f070003;
+        /**  The separator character for breadcrumbs.  For most left to right languages, this shouldn't need to be changed. 
+         */
+        public static final int twofortyfouram_locale_breadcrumb_separator=0x7f070004;
+        /**  The button that performs a search for the app on the Android Market.  MDPI 17 character maximum 
+         */
+        public static final int twofortyfouram_locale_marketactivity_dialog_button_search=0x7f070007;
+        /**  The primary message of the MarketActivity dialog.  There is no character length maximum 
+         */
+        public static final int twofortyfouram_locale_marketactivity_dialog_message=0x7f070006;
+        /**  The title of a dialog informing the user that Locale isn't installed.  MDPI 21 character maximum. 
+         */
+        public static final int twofortyfouram_locale_marketactivity_dialog_title=0x7f070005;
+        /**  The Don't Save button in the menu.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_dontsave=0x7f070008;
+        /**  The Help button in the menu.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_help=0x7f070009;
+        /**  The Save button in the menu.  MDPI 21 character maximum 
+         */
+        public static final int twofortyfouram_locale_menu_save=0x7f07000a;
     }
     public static final class style {
-        public static final int Theme_asksven=0x7f070000;
-        public static final int Theme_asksven_Fullscreen=0x7f070002;
-        public static final int Theme_asksven_NoTitleBar=0x7f070001;
+        /**  Style for all standard Locale and Locale plug-in Activity instances 
+         */
+        public static final int Theme_Locale_Dark=0x7f080000;
+        /**  Style for Activities that look like a dialog 
+         */
+        public static final int Theme_Locale_Dialog=0x7f080002;
+        /**  Style for content within the twofortyfouram_locale_border.9 frame 
+         */
+        public static final int Theme_Locale_Light=0x7f080001;
+        public static final int Theme_asksven=0x7f080003;
+        public static final int Theme_asksven_Fullscreen=0x7f080005;
+        public static final int Theme_asksven_NoTitleBar=0x7f080004;
     }
     public static final class xml {
         public static final int preferences=0x7f040000;
---
+++ src/com/asksven/betterlatitude/localeplugin/receiver/FireReceiver.java
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2012 asksven
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * This file was contributed by two forty four a.m. LLC <http://www.twofortyfouram.com>
+ * unter the terms of the Apache License, Version 2.0
+ */
+
+package com.asksven.betterlatitude.localeplugin.receiver;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.asksven.betterlatitude.LocationService;
+import com.asksven.betterlatitude.localeplugin.Constants;
+import com.asksven.betterlatitude.localeplugin.bundle.BundleScrubber;
+import com.asksven.betterlatitude.localeplugin.bundle.PluginBundleManager;
+import com.asksven.betterlatitude.localeplugin.ui.EditActivity;
+
+/**
+ * This is the "fire" BroadcastReceiver for a Locale Plug-in setting.
+ */
+public final class FireReceiver extends BroadcastReceiver
+{
+
+    /**
+     * @param context {@inheritDoc}.
+     * @param intent the incoming {@link com.twofortyfouram.locale.Intent#ACTION_FIRE_SETTING} Intent. This should contain the
+     *            {@link com.twofortyfouram.locale.Intent#EXTRA_BUNDLE} that was saved by {@link EditActivity} and later broadcast
+     *            by Locale.
+     */
+    @Override
+    public void onReceive(final Context context, final Intent intent)
+    {
+        /*
+         * Always be sure to be strict on input parameters! A malicious third-party app could always send an empty or otherwise
+         * malformed Intent. And since Locale applies settings in the background, the plug-in definitely shouldn't crash in the
+         * background.
+         */
+
+        /*
+         * Locale guarantees that the Intent action will be ACTION_FIRE_SETTING
+         */
+        if (!com.twofortyfouram.locale.Intent.ACTION_FIRE_SETTING.equals(intent.getAction()))
+        {
+            if (Constants.IS_LOGGABLE)
+            {
+                Log.e(Constants.LOG_TAG, String.format("Received unexpected Intent action %s", intent.getAction())); //$NON-NLS-1$
+            }
+            return;
+        }
+
+        /*
+         * A hack to prevent a private serializable classloader attack
+         */
+        BundleScrubber.scrub(intent);
+        BundleScrubber.scrub(intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE));
+
+        final Bundle bundle = intent.getBundleExtra(com.twofortyfouram.locale.Intent.EXTRA_BUNDLE);
+
+        // TODO add processing code here
+        Log.i(Constants.LOG_TAG, "Preparing to call ALTitude with action: " + bundle.getString(PluginBundleManager.BUNDLE_EXTRA_INT_ACTION));
+        
+        int iAction = bundle.getInt(PluginBundleManager.BUNDLE_EXTRA_INT_ACTION);
+        int iInterval = bundle.getInt(PluginBundleManager.BUNDLE_EXTRA_INT_INTERVAL);
+        int iAccuracy = bundle.getInt(PluginBundleManager.BUNDLE_EXTRA_INT_ACCURACY);
+        int iDuration = bundle.getInt(PluginBundleManager.BUNDLE_EXTRA_INT_DURATION);
+        
+        if (iAction == 0)
+        {
+			LocationService.getInstance().setQuickChange(iInterval, iAccuracy, iDuration);
+        }
+        else
+        {
+        	LocationService.getInstance().resetQuickChange();
+        }
+        
+    }
+}
