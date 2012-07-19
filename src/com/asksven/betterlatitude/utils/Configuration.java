36017ecbc9be571ec9c8fd640ba0e9d115480797
asksven <sven.knispel@gmail.com>
2012-07-19 19:36:09 +0200

    Fixed #61: [bug] on JB license not recognised https://github.com/asksven/ALTitude/issues/issue/61

--- src/com/asksven/betterlatitude/utils/Configuration.java
+++ src/com/asksven/betterlatitude/utils/Configuration.java
@@ -19,6 +19,8 @@ import java.lang.reflect.Field;
 import java.lang.reflect.Method;
 
 import android.content.Context;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.os.Build;
 import android.os.IBinder;
 import android.os.Parcel;
 import android.os.Parcelable;
@@ -26,50 +28,59 @@ import android.util.Log;
 
 /**
  * Helper class that returns info about the configuration (free of full)
+ * 
  * @author sven
- *
+ * 
  */
-public class Configuration
-{
+public class Configuration {
 	final static String TAG = "Configuration";
-	
-	public static boolean isFullVersion(Context ctx)
-	{
-		return (getVersion(ctx) > 0);
+
+	public static boolean isFullVersion(Context ctx) {
+		return (getVersion(ctx) >= 0);
 	}
-	
-	private static int getVersion(Context ctx)
-	{
+
+	private static int getVersion(Context ctx) {
 		int iRet = 0;
 		// try accessing the License class by reflection
-		try
-		{
-	          // ClassLoader cl = ctx.getClassLoader();
-	          Context foreignContext = ctx.createPackageContext(
-	        		  "com.asksven.betterlatitude_license",
-	        		  Context.CONTEXT_INCLUDE_CODE |
-	        		  Context.CONTEXT_IGNORE_SECURITY);
-	          Class<?> c =
-	        		  foreignContext.getClassLoader().loadClass("com.asksven.betterlatitude.configuration.License");
-	         
-	          Method methodGetVersion = c.getMethod("getVersion");
-	          	     
-	          iRet = (Integer) methodGetVersion.invoke(c); 
+		try {
+			// ClassLoader cl = ctx.getClassLoader();
+			Context foreignContext = ctx.createPackageContext(
+					"com.asksven.betterlatitude_license",
+					Context.CONTEXT_INCLUDE_CODE
+							| Context.CONTEXT_IGNORE_SECURITY);
 
-	    }
-		catch( IllegalArgumentException e )
+			if (Build.VERSION.SDK_INT <= 15)
+			{
+
+				Class<?> c = foreignContext.getClassLoader().loadClass(
+						"com.asksven.betterlatitude.configuration.License");
+
+				Method methodGetVersion = c.getMethod("getVersion");
+
+				iRet = (Integer) methodGetVersion.invoke(c);
+			} else {
+				iRet = 0;
+			}
+
+		}
+		catch (IllegalArgumentException e)
 		{
-	        iRet = -1;
-	    }
+			iRet = -1;
+		}
 		catch (ClassNotFoundException e)
 		{
-	    	iRet = -2;
-	    }
-		catch( Exception e )
+			iRet = -2;
+		}
+		catch (NameNotFoundException e)
 		{
-	    	iRet = -3;
-	    }    
-		if (iRet > 0)
+			iRet = -4;
+		}
+		catch (Exception e)
+		{
+			iRet = -3;
+		}
+		
+		if (iRet >= 0)
 		{
 			Log.i(TAG, "license was detected");
 		}
@@ -77,7 +88,7 @@ public class Configuration
 		{
 			Log.i(TAG, "no license was detected: " + iRet);
 		}
-		
+
 		return iRet;
 	}
 }
